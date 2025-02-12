566dc38928bcad3bb7c8beb9b3c8f9ea320f29aa
Jessica Dong <jessicad.7373@gmail.com>
2025-02-12 18:25:08 -0500

    index.py

--- api/index.py
+++ api/index.py
@@ -95,42 +95,5 @@ def extract_tables_with_gmft():
     doc.close()
 
 
-detector = AutoTableDetector()
-formatter = AutoTableFormatter()
-
-def ingest_pdf(pdf_path):
-    doc = PyPDFium2Document(pdf_path)
-    tables = []
-    for page in doc:
-        tables += detector.extract(page)
-    return tables, doc
-
-@app.route('/api/extract-tables', methods=['POST'])
-def extract_tables_with_gmft():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file provided."}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "No selected file."}), 400
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        pdf_path = os.path.join(temp_dir, file.filename)
-        file.save(pdf_path)
-
-        try:
-            tables, doc = ingest_pdf(pdf_path)
-
-            if tables:
-                ft = formatter.extract(tables[0])
-                return jsonify({"message": f"{len(tables)} \n {ft.df()}"})
-            else:
-                print("No tables found in the PDF.")
-                return jsonify({"message": "No tables found in the PDF."})
-        except Exception as e:
-            return jsonify({"error": f"Failed to extract tables: {str(e)}"}), 500
-    
-    doc.close()
-
 if __name__ == '__main__':
     app.run(port=5328)
